83a39d23d3ccce33aff3def3c2509ca87605cb8a
Herschelle42 <github@threemillion.net>
2019-06-27 17:00:52 +1000

    Update params and add additional example systems
    Added additional comments and ideas
    Update params to pass to Tests

--- vRA-Infrastructure-Health-Check.Tests.Wrapper.ps1
+++ vRA-Infrastructure-Health-Check.Tests.Wrapper.ps1
@@ -49,9 +49,7 @@ Password
 NOTE: Backslashes as values are escape characters. Therefore user\domain becomes user\\domain
 
 
-TODO:  Add Log Insight etc to the list...
-       Add NSX to the list?
-       SSH to vCenter for service status?  eg vsphere-ui - although we do not own the password
+TODO:  SSH to vCenter for service status?  eg vsphere-ui - although we do not own the password
        SSH to vRA
        WinRM to IaaS Manager and Web servers to check Services etc
 
@@ -64,152 +62,318 @@ $jsonData = @"
             "Type": "vRA",
             "FQDN": "vra-app-prd.management.corp.local",
             "Port": "443",
+            "Protocol": "https",
             "api": "",
             "vamiPort": "5480",
-            "tenant": "vsphere.local"
+            "tenant": "vsphere.local",
+            "healthAPI": ""
         },
         {
             "Type": "vRO",
             "FQDN": "vra-app-prd.management.corp.local",
             "Port": "443",
-            "api": "/vco/api"
+            "Protocol": "https",
+            "api": "/vco/api",
+            "healthAPI": ""
         },
         {
             "Type": "IaaSMgr",
             "FQDN": "vra-mgr-prd.management.corp.local",
             "Port": "",
+            "Protocol": "https",
+            "api": "",
             "healthAPI": "/VMPS2"
         },
         {
             "Type": "IaaSWeb",
             "FQDN": "vra-web-prd.management.corp.local",
             "Port": "",
+            "Protocol": "https",
+            "api": "",
             "healthAPI": "/WAPI/api/status"
+        },
+        {
+            "Type":  "LogInsight",
+            "FQDN":  "vrli1-dc1",
+            "Port": "",
+            "Protocol": "https",
+            "api": "",
+            "healthAPI": "",
+            "IgnoreCert": "true"
+        },
+        {
+            "Type":  "NSXT",
+            "FQDN":  "nsxmvp1-dc1",
+            "Port": "",
+            "Protocol": "https",
+            "api": "",
+            "healthAPI": ""
+        },
+        {
+            "Type":  "NSXT",
+            "FQDN":  "nsxmvp1-cbr2",
+            "Port": "",
+            "Protocol": "https",
+            "api": "",
+            "healthAPI": ""
         }
     ],
     "Server": [
-            {
-                "Type":  "vCenter",
-                "Hostname":  "corpvc1-dc1",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.164.58.21",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "vCenter",
-                "Hostname":  "corpvc1-dc2",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.164.186.21",
-                "DataCenter": "DC2"
-            },
-            {
-                "Type":  "vCenter",
-                "Hostname":  "vcenter",
-                "DNSDomain":  "sddc-13-211-5-135.vmwarevmc.com",
-                "IPAddress":  "192.163.174.4",
-                "DataCenter": "VMC"
-            },
-            {
-                "Type":  "IaaSMgr",
-                "Hostname":  "iaas-dc1",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.19",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "IaaSMgr",
-                "Hostname":  "iaas-dc2",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.20",
-                "DataCenter": "DC2"
-            },
-            {
-                "Type":  "IaaSWeb",
-                "Hostname":  "iaasweb-dc1",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.21",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "IaaSWeb",
-                "Hostname":  "iaasweb-dc2",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.22",
-                "DataCenter": "DC2"
-            },
-            {
-                "Type":  "vRA",
-                "Hostname":  "vra01",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.16",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "vRA",
-                "Hostname":  "vra02",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.17",
-                "DataCenter": "DC2"
-            },
-            {
-                "Type":  "VRA",
-                "Hostname":  "vra03",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.1.18",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "SQL",
-                "Hostname":  "corpsql-dc1",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.48.26",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "SQL",
-                "Hostname":  "corpsql-dc2",
-                "DNSDomain":  "management.corp.local",
-                "IPAddress":  "192.162.48.29",
-                "DataCenter": "DC2"
-            },
-            {
-                "Type":  "AD",
-                "Hostname":  "corpdc-dc1",
-                "DNSDomain":  "myac.gov.au",
-                "IPAddress":  "192.164.63.1",
-                "DataCenter": "DC1"
-            },
-            {
-                "Type":  "AD",
-                "Hostname":  "corpdc-dc2",
-                "DNSDomain":  "myac.gov.au",
-                "IPAddress":  "192.164.191.1",
-                "DataCenter": "DC2"
-            },
-            {
-                "Type":  "AD",
-                "Hostname":  "corpdc-vmc",
-                "DNSDomain":  "myac.gov.au",
-                "IPAddress":  "192.163.191.1",
-                "DataCenter": "VMC"
-            }
+        {
+            "Type":  "vCenter",
+            "Hostname":  "vc1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.21",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "vCenter",
+            "Hostname":  "vc1-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.21",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "vCenter",
+            "Hostname":  "vcenter",
+            "DNSDomain":  "sddc.vmwarevmc.com",
+            "IPAddress":  "192.163.174.4",
+            "DataCenter": "VMC"
+        },
+        {
+            "Type":  "IaaSMgr",
+            "Hostname":  "dc1wprdvdp01",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.19",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "IaaSMgr",
+            "Hostname":  "dc2wprdvdp01",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.20",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "IaaSWeb",
+            "Hostname":  "dc1wprdvwm01",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.21",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "IaaSWeb",
+            "Hostname":  "dc2wprdvwm01",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.22",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "vRA",
+            "Hostname":  "dc1aprdvaa01",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.16",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "vRA",
+            "Hostname":  "dc2aprdvaa02",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.17",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "VRA",
+            "Hostname":  "dc1aprdvaa03",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.1.18",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "SQL",
+            "Hostname":  "dc1wprdsql04",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.48.26",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "SQL",
+            "Hostname":  "dc2wprdsql04",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.162.48.29",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "AD",
+            "Hostname":  "DC1WPRDADS1",
+            "DNSDomain":  "subdomain.corp.local",
+            "IPAddress":  "192.164.63.1",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "AD",
+            "Hostname":  "DC2WPRDADS1",
+            "DNSDomain":  "subdomain.corp.local",
+            "IPAddress":  "192.164.191.1",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "AD",
+            "Hostname":  "DC3WPRDADS1",
+            "DNSDomain":  "subdomain.corp.local",
+            "IPAddress":  "192.163.191.1",
+            "DataCenter": "VMC"
+        },
+        {
+            "Type":  "AD",
+            "Hostname":  "DC1WPPDADS1",
+            "DNSDomain":  "nonprod.subdomain.corp.local",
+            "IPAddress":  "192.164.63.17",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "AD",
+            "Hostname":  "DC2WPPDADS1",
+            "DNSDomain":  "nonprod.subdomain.corp.local",
+            "IPAddress":  "192.164.191.17",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "AD",
+            "Hostname":  "DC3WPPDADS1",
+            "DNSDomain":  "nonprod.subdomain.corp.local",
+            "IPAddress":  "192.163.191.17",
+            "DataCenter": "VMC"
+        },
+        {
+            "Type":  "LogInsight",
+            "Hostname":  "vrlim1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.25",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "LogInsight",
+            "Hostname":  "vrliw1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.26",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "LogInsight",
+            "Hostname":  "vrliw2-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.27",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "NSXTMgr",
+            "Hostname":  "nsxm1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.31",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "NSXTMgr",
+            "Hostname":  "nsxm2-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.32",
+            "DataCenter": "DC1"
+        },        
+        {
+            "Type":  "NSXTMgr",
+            "Hostname":  "nsxm3-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.33",
+            "DataCenter": "DC1"
+        },       
+        {
+            "Type":  "NSXTMgr",
+            "Hostname":  "nsxm1-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.31",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "NSXTMgr",
+            "Hostname":  "nsxm2-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.32",
+            "DataCenter": "CBR2"
+        },        
+        {
+            "Type":  "NSXTMgr",
+            "Hostname":  "nsxm3-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.33",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "vRNI",
+            "Hostname":  "vrnipt1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.60",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "vRNI",
+            "Hostname":  "vrnipt1-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.60",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "vRNI",
+            "Hostname":  "vrnipx1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.61",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "vRNI",
+            "Hostname":  "vrnipx1-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.61",
+            "DataCenter": "CBR2"
+        },
+        {
+            "Type":  "vRNI",
+            "Hostname":  "vrnipx1-vmc",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.163.191.61",
+            "DataCenter": "VMC"
+        },
+        {
+            "Type":  "vIDM",
+            "Hostname":  "vidm1-dc1",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.58.55",
+            "DataCenter": "DC1"
+        },
+        {
+            "Type":  "vIDM",
+            "Hostname":  "vidm1-cbr2",
+            "DNSDomain":  "management.corp.local",
+            "IPAddress":  "192.164.186.55",
+            "DataCenter": "CBR2"
+        }
     ],
     "Template": [
 
         {
             "Type": "IaaS",
             "OS": "windows2016",
-            "Name": "corp_win16_std_20190611_1414_template"
+            "Name": "win16_std_20190611_1414_template"
         },
         {
             "Type": "IaaS",
             "OS": "windows2012r2",
-            "Name": "corp_win12r2_std_20190612_1711_template"
+            "Name": "win12r2_std_20190612_1711_template"
         },
         {
             "Type": "IaaS",
             "OS": "rhel7",
-            "Name": "corp_rhel75_20190514_927dd36e_template"
+            "Name": "rhel75_20190514_927dd36e_template"
         }
     ]
 
@@ -224,14 +388,17 @@ Add any variables you want passed into the Pester test script. The test script
 must be set up to receive parameters (variables) like a function would be.
 #>
 $Params = @{
-    #myVar = "myValue";
-    CredMgmt = $credMgmt;
+    #ParamName = "myParamValue";
+    Credential = $credMgmt;
     JSONPayload = $jsonData;
     ICMP = $true;
     vCenter = $true;
     vRA = $true;
     vRO = $true;
     vRAIaaS = $true;
+    NSXT = $true;
+    LogInsight = $true;
+
 }
 
 #To save the result as an NUnit file add the following :  -OutputFile $pesterOutput -OutputFormat NUnitXml 
@@ -242,14 +409,18 @@ $VerbosePreference = "SilentlyContinue"
 Return
 <#
 
+
 Service JSON Template
         {
             "Type": "",
             "FQDN": "",
             "Port": "",
+            "Protocol": "",
             "api": "",
             "vamiPort": "",
-            "tenant": ""
+            "tenant": "",
+            "HealthAPI": "",
+            "IgnoreCert": "true"
         },
 
 Service designed for load balanced systems or points of entry.
@@ -258,13 +429,17 @@ ups for load balancers. could be added as another item and if IP Address
 is set, then test it. If not then skip it.
 tenant is only used by vRA at this point.
 
+IgnoreCert is for those servers where certificate replacement has not been done
+or the system running the Pester tests does not trust the certificate.
+Not fully implemented yet. - Only done for LogInsight
+
 
 Server JSON template
 
             {
                 "Type":  "",
                 "Hostname":  "",
-                "DNSDomain":  "myac.gov.au",
+                "DNSDomain":  "subdomain.corp.local",
                 "IPAddress":  "10.",
                 "DataCenter": ""
             }
@@ -281,6 +456,17 @@ Break out template into new section? ie out of vCenter?
 as not all environments will be replicating to all vCenters?
 Possibly add an optional vCenter value. if set then only check
 this vCenter. Or may have to be an array of vCenters.
+
+
+Add new section of health checking to test Ports?
+Port scanning by telnet?
+If wanting health check multiple ports?
+Add extra server config
+or
+Make into array of ports. then add some logic to handle single vs array
+or
+Create a port scan section in JSON payload.
+
 
 #>
 
